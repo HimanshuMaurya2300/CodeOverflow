5cff8098661e3f7e6fba8b73d8caf698bfeebac6
Himanshu <mauryah380@gmail.com>
2024-04-04 02:31:23 +0530

    fixed dynamic route of question page

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -7,12 +7,13 @@ import Votes from '@/components/shared/Votes'
 import { getQuestionById } from '@/lib/actions/question.action'
 import { getUserById } from '@/lib/actions/user.action'
 import { formatAndDivideNumber, getTimeStamp } from '@/lib/utils'
+import { URLProps } from '@/types'
 import { auth } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-const page = async ({ params, searchParams }) => {
+const page = async ({ params, searchParams }: URLProps) => {
 
     // console.log(params, searchParams)
     const result = await getQuestionById({ questionId: params.id })
