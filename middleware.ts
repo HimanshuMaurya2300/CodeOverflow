6db3f7a8e80b2df994ee75d48e2303c16cf2b32f
Himanshu <mauryah380@gmail.com>
2024-04-04 11:59:14 +0530

    added webook to public routes

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,20 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
     publicRoutes: [
-        "/"
+        "/",
+        '/api/webhook',
+        '/question/:id',
+        '/tags',
+        '/tags/:id',
+        '/profile/:id',
+        '/community',
+        '/jobs',
+        '/rooms'
     ],
+    ignoredRoutes: [
+        "/api/webhook",
+        "/api/chatgpt"
+    ]
 });
 
 export const config = {
